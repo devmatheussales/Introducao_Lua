b6233092c37d47803656efdab1765c507bbd60c6
Sales <157637433+devmatheussales@users.noreply.github.com>
2025-01-27 11:12:26 -0300

    Add aula01-numeros.lua and aula01-operadores-comparacao.lua

---
+++ aulas/aula01-numeros.lua
@@ -0,0 +1,15 @@
+--Mostra algumas operações matemáticas básicas
+print("A seguir, vamos somar: 33+77")
+print(33 + 77)
+
+print("A seguir, vamos Subtrair: 10-7")
+print(10 - 7)
+
+print("A seguir, vamos Multiplicar: 13x2")
+print(13*2)
+
+print("A seguir, vamos Dividir: 144/12")
+print(144 / 12)
+
+print("A seguir, vamos achar o Resto de: 21/3 ")
+print(21%3)
