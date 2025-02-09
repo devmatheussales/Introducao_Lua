a020b7488667bc9313647e08f6b405e24dcda3c5
Sales <157637433+devmatheussales@users.noreply.github.com>
2025-02-09 18:14:09 -0300

    Add aula12.lua

---
+++ aulas/aula12-not.lua
@@ -0,0 +1,10 @@
+
+
+-- O operador 'not' inverte o valor seguinte.
+if not false then
+    print("not false")
+end
+
+if not true then
+    print("not true")
+end
---
+++ aulas/aula12-or.lua
@@ -0,0 +1,12 @@
+
+function contarPontos(nome)
+    if(nome == "Tartaruga" or nome == "Planta") then
+        return 100
+    else
+        return 50
+    end
+end
+
+print(contarPontos("Tartaruga"))
+print(contarPontos("Bloco"))
+print(contarPontos("Planta"))
